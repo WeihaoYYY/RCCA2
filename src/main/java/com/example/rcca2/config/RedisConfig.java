cb5afcb9403ff5d014d12ab67722aecd4e129d75
WeihaoYYY <yue032994@outlook.com>
2024-11-28 22:33:51 +1100

    connection testing 5

--- src/main/java/com/example/rcca2/config/RedisConfig.java
+++ src/main/java/com/example/rcca2/config/RedisConfig.java
@@ -1,12 +1,17 @@
 package com.example.rcca2.config;
 
 import com.example.rcca2.Utils.FastJsonRedisSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class RedisConfig {
@@ -36,4 +41,23 @@ public class RedisConfig {
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory();
     }
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
+
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @PostConstruct
+    public void checkRedisConnection() {
+        try {
+            RedisConnection connection = redisConnectionFactory.getConnection(); // 使用实例调用 getConnection()
+            connection.ping();
+            System.out.println("Redis连接成功");
+        } catch (Exception e) {
+            System.err.println("Redis连接失败: " + e.getMessage());
+        }
+    }
+
 }
